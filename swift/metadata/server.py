a8616e21d28a00a39a6722c41073f55b79dcb138
Nick Wood <nickswift498@gmail.com>
2014-02-14 14:26:03 -0800

    extending DB broker

--- swift/metadata/server.py
+++ swift/metadata/server.py
@@ -2,3 +2,35 @@
 server.py 
 Metadata Database Broker
 '''
+
+from swift.common.db import DatabaseBroker
+
+# Interface with metadata database
+class MetaDatabase(DatabaseBroker):
+
+    # Initialize DB
+    def initialize(self, *args, **kwargs):
+        super(self).initialize(*args, **kwargs)
+
+        # TODO: discuss content of schema
+        self.conn.executescript("""
+            CREATE TABLE account_metadata (
+            );
+            CREATE TABLE object_metadata (
+            );
+            CREATE TABLE container_metadata (
+            );
+        """)
+
+    '''
+    Insert adds rows if they don't exist, and updates them when they do 
+    exist.
+    '''
+    # Accounts 
+    def insertAccount(self, data):
+
+    # Objects
+    def insertObject(self, data):
+
+    # Containers
+    def insertContainer(self, data):
