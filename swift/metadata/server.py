6659e3de3e21922902e26fe76fe4e2d0ef11558e
Nick Wood <nickswift498@gmail.com>
2014-02-24 22:24:13 -0800

    expanding metadata server
    The metadata server should follow the other server types fairly closely
    with an exception: we're only supporting SQL here.

--- swift/metadata/server.py
+++ swift/metadata/server.py
@@ -1,149 +1,111 @@
-'''
-server.py 
-Metadata Database Broker
-'''
 
-from swift.common.db import DatabaseBroker
+import os, time, traceback
+from datetime import datetime
+from swift import gettext_ as _
+from xml.etree.cElementTree import Element, SubElement, tostring
 
-# Interface with metadata database
-class MetaDatabase(DatabaseBroker):
+from eventlet import Timeout
+import swift.common.db
 
-    # Initialize DB
-    def _initialize(self, conn, put_timestamp):
-        # Create metadata tables
-        self.create_account_md_table(conn)
-        self.create_container_md_table(conn)
-        self.create_object_md_table(conn)
+from swift.metadata.backend import MetadataBroker
+from swift.common.db import DatabaseAlreadyExists
+from swift.common.request_helpers import get_param, \
+    get_listing_content_type, split_and_validate_path, is_sys_or_user_meta
 
-    def create_account_md_table(self, conn):
-        conn.executescript("""
-            CREATE TABLE account_metadata (
-                ROWID INTEGER PRIMARY KEY AUTOINCREMENT,
-                account_uri TEXT,
-                account_name TEXT UNIQUE,
-                account_tenant_id TEXT,
-                account_first_use_time TEXT DEFAULT '0',
-                account_last_modified_time TEXT DEFAULT '0',
-                account_last_changed_time TEXT DEFAULT '0',
-                account_delete_time TEXT DEFAULT '0',
-                account_last_activity_time TEXT DEFAULT '0',
-                account_container_count INTEGER,
-                account_object_count INTEGER,
-                account_bytes_used INTEGER,
-                account_meta TEXT
-            );
-            
-        """)
-    def create_container_md_table(self, conn):
-        conn.executescript("""
-            CREATE TABLE container_metadata (
-                ROWID INTEGER PRIMARY KEY AUTOINCREMENT,
-                container_uri TEXT,
-                container_name TEXT UNIQUE,
-                container_account_name TEXT,
-                container_create_time TEXT DEFAULT '0',
-                container_last_modified_time TEXT DEFAULT '0',
-                container_last_changed_time TEXT DEFAULT '0',
-                container_delete_time TEXT DEFAULT '0',
-                container_last_activity_time TEXT DEFAULT '0',
-                container_read_permissions TEXT,
-                container_write_permissions TEXT,
-                container_sync_to TEXT,
-                container_sync_key TEXT,
-                container_versions_location TEXT,
-                container_object_count INTEGER,
-                container_bytes_used INTEGER,
-                container_meta TEXT
-            );
+from swift.common.utils import get_logger, hash_path, public, normalize_timestamp, storage_directory, \
+    validate_sync_to, config_true_value, json, timing_stats, replication, override_bytes_from_content_type
 
-            CREATE UNIQUE INDEX uid_container ON container_metadata(ROWID);
-        """)
+from swift.common.constraints import CONTAINER_LISTING_LIMIT, check_mount, check_float, check_utf8
+from swift.common.bufferedhttp import http_connect
+from swift.common.exceptions import ConnectionTimeout
+from swift.common.db_replicator import ReplicatorRpc
+from swift.common.http import HTTP_NOT_FOUND, is_success
 
-    def create_object_md_table(self, conn):
+from swift.common.swob import HTTPAccepted, HTTPBadRequest, HTTPConflict, HTTPCreated, HTTPInternalServerError,\
+    HTTPNoContent, HTTPNotFound,HTTPPreconditionFailed, HTTPMethodNotAllowed, Request, Response, \
+    HTTPInsufficientStorage, HTTPException, HeaderKeyDict
 
-        conn.executescript("""
-            CREATE TABLE object_metadata (
-                ROWID INTEGER PRIMARY KEY AUTOINCREMENT,
-                object_uri TEXT,
-                object_name TEXT UNIQUE,
-                object_account_name TEXT,
-                object_container_name TEXT,
-                object_location TEXT,
-                object_uri_create_time TEXT DEFAULT '0',
-                object_last_modified_time TEXT DEFAULT '0',
-                object_last_changed_time TEXT DEFAULT '0',
-                object_delete_time TEXT DEFAULT '0',
-                object_last_activity_time TEXT DEFAULT '0',
-                object_etag_hash TEXT,
-                object_content_type TEXT,
-                object_content_length INTEGER,
-                object_content_encoding TEXT,
-                object_content_disposition TEXT,
-                object_content_language TEXT,
-                object_cache_control TEXT,
-                object_delete_at TEXT DEFAULT '0',
-                object_manifest_type INTEGER,
-                object_manifest TEXT,
-                object_access_control_allow_origin TEXT,
-                object_access_control_allow_credentials TEXT,
-                object_access_control_expose_headers TEXT,
-                object_access_control_max_age TEXT,
-                object_access_control_allow_methods TEXT,
-                object_access_control_allow_headers TEXT,
-                object_origin TEXT,
-                object_access_control_request_method TEXT,
-                object_access_control_request_headers TEXT,
-                object_meta TEXT
-            );
-        """)
+DATADIR = 'metadata'
 
-    # Insert metadata into the DB
-    def insert_account_md(self, conn, data):      
-        # Query template
-        query = """
-            INSERT INTO account_metadata (
-                account_uri,
-                account_name,
-                account_tenant_id,
-                account_first_use_time,
-                account_last_modified_time,
-                account_last_changed_time,
-                account_delete_time,
-                account_last_activity_time,
-                account_container_count,
-                account_object_count,
-                account_bytes_used,
-                account_meta
-            )
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%d,%d,%d,%s)
-            ON DUPLICATE KEY UPDATE
-                account_uri                = VALUES(account_uri),
-                account_name               = VALUES(account_name),
-                account_tenant_id          = VALUES(account_tenant_id),
-                account_first_use_time     = VALUES(account_first_use_time),
-                account_last_modified_time = VALUES(account_last_modified_time),
-                account_last_changed_time  = VALUES(account_last_changed_time),
-                account_delete_time        = VALUES(account_delete_time),
-                account_last_activity_time = VALUES(account_last_activity_time),
-                account_container_count    = VALUES(account_container_count),
-                account_object_count       = VALUES(account_object_count),
-                account_bytes_used         = VALUES(account_bytes_used),
-                account_meta               = VALUES(account_meta)
-            ;
+class MetadataController(object):
+    # WSGI Controller for metadata server
+    save_headers = [
+        'x-metadata-read',
+        'x-metadata-write',
+        'x-metadata-sync-key',
+        'x-metadata-sync-to'
+    ]
+
+    def __init__(self, conf, logger=None):
+        self.logger = logger or get_logger(conf, log_route='metadata-server')
+        self.root = conf.get('devices', '/srv/node')
+        self.mount_check = config_true_value(conf.get('mount_check', 'true'))
+        self.node_timeout = int(conf.get('node_timeout', 3))
+        self.conn_timeout = float(conf.get('node_timeout', 3))
+        replication_server = conf.get('replication_server', None)
+        if replication_server is not None:
+            replication_server = config_true_value(replication_server)
+        self.replication_server = replication_server
+        self.allowed_sync_hosts = [
+            h.strip()
+            for h in conf.get('allowed_sync_hosts', '127.0.0.1').split(',')
+            if h.strip()
+        ]
+        self.replicator_rpc = ReplicatorRpc(
+            self.root,
+            DATADIR,
+            MetadataBroker,
+            self.mount_check,
+            logger=self.logger
+        )
+        self.auto_create_account_prefix = conf.get('auto_create_account_prefix') or '.'
+        if config_true_value(conf.get('allow_versions', 'f')):
+            self.save_headers.append('x-versions-location')
+        swift.common.db.DB_PREALLOCATION = config_true_value(conf.get('db_preallocation', 'f'))
+
+    def _get_metadata_broker(self, drive, part, account, container, **kwargs):
         """
-        # Build and execute query for each requested insertion
-        for item in data:
-            formatted_query = query % (
-                item['account_uri'],
-                item['account_name'],
-                item['account_tenant_id'],
-                item['account_first_use_time'],
-                item['account_last_modified_time'],
-                item['account_last_changed_time'],
-                item['account_delete_time'],
-                item['account_last_activity_time'],
-                item['account_container_count'],
-                item['account_bytes_used'],
-                item['account_meta']            
-            )
-            conn.executescript(formatted_query)
+        Get a DB broker for the metadata
+        """
+        hash = hash_path(account, container)
+        db_dir = storage_directory(DATADIR, part, hash)
+        db_path = os.path.join(self.root, drive, drive)
+        kwargs.setdefault('account', account)
+        kwargs.setdefault('container', container)
+        kwargs.setdefault('logger', self.logger)
+        return MetadataBroker(**kwargs)
+
+    '''
+    # Implementation WIP
+    @public
+    @timing_stats
+    def DELETE(self, req):
+        # Handle HTTP DELETE requests
+
+    @public
+    @timing_stats
+    def PUT(self, req):
+        # Handle HTTP PUT requests
+
+    @public
+    @timing_stats
+    def HEAD(self, req):
+        # Handle HTTP HEAD requests
+
+    @public
+    @timing_stats
+    def GET(self, req):
+        # Handle HTTP GET requests
+
+    @public
+    @timing_stats
+    def POST(self, req):
+        # Handle HTTP POST requests
+
+    '''
+
+def app_factory(global_conf, **local_conf):
+    """paste.deploy app factory for creating WSGI container server apps"""
+    conf = global_conf.copy()
+    conf.update(local_conf)
+    return MetadataController(conf)
