675918553864b90de5eb4f6113dbccc3ba02f093
Nick Wood <nickswift498@gmail.com>
2014-02-26 13:55:11 -0800

    adding insertion methods for containers and objects

--- swift/metadata/backend.py
+++ swift/metadata/backend.py
@@ -103,7 +103,8 @@ class MetadataBroker(DatabaseBroker):
             );
         """)
 
-    # Query template
+    # Data insertion methods
+
     def insert_account_md(self, data):
         with self.get() as conn:
             query = """
@@ -151,6 +152,177 @@ class MetadataBroker(DatabaseBroker):
                     item['account_container_count'],
                     item['account_bytes_used'],
                     item['account_meta']
+                )
+                conn.executescript(formatted_query)
+
+    def insert_container(self, data):
+        query = '''
+            INSERT INTO container_metadata (
+                container_uri,
+                container_name,
+                container_account_name,
+                container_create_time,
+                container_last_modified_time,
+                container_last_changed_time,
+                container_delete_time,
+                container_last_activity_time,
+                container_read_permissions,
+                container_write_permissions,
+                container_sync_to,
+                container_sync_key,
+                container_versions_location,
+                container_object_count INTEGER,
+                container_bytes_used INTEGER,
+                container_meta
+            )
+            VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                %s, %d, %d, %s 
+            )
+            ON DUPLICATE KEY UPDATE 
+                container_uri = VALUES(container_uri),
+                container_name = VALUES(container_name),
+                container_account_name = VALUES(container_account_name),
+                container_create_time = VALUES(container_create_time),
+                container_last_modified_time = VALUES(container_last_modified_time),
+                container_last_changed_time = VALUES(container_last_changed_time),
+                container_delete_time = VALUES(container_delete_time),
+                container_last_activity_time = VALUES(container_last_activity_time),
+                container_read_permissions = VALUES(container_read_permissions),
+                container_write_permissions = VALUES(container_write_permissions),
+                container_sync_to = VALUES(container_sync_to),
+                container_sync_key = VALUES(container_sync_key),
+                container_versions_location = VALUES(container_versions_location),
+                container_object_count INTEGER = VALUES(container_object_count),
+                container_bytes_used INTEGER = VALUES(container_bytes_used),
+                container_meta = VALUES(container_meta)
+            ;
+        '''
+        for item in data:
+            formatted_query = query % (
+                item['container_uri'],
+                item['container_name'],
+                item['container_account_name'],
+                item['container_create_time'],
+                item['container_last_modified_time'],
+                item['container_last_changed_time'],
+                item['container_delete_time'],
+                item['container_last_activity_time'],
+                item['container_read_permissions'],
+                item['container_write_permissions'],
+                item['container_sync_to'],
+                item['container_sync_key'],
+                item['container_versions_location'],
+                item['container_object_count INTEGER'],
+                item['container_bytes_used INTEGER'],
+                item['container_meta']
+            )
+            conn.executescript(formatted_query)
+
+    def insert_object_md(self, data):
+        with self.get() as conn:
+            query = '''
+                INSERT INTO object_metadata (
+                    object_uri,
+                    object_name,
+                    object_account_name,
+                    object_container_name,
+                    object_location,
+                    object_uri_create_time,
+                    object_last_modified_time,
+                    object_last_changed_time,
+                    object_delete_time,
+                    object_last_activity_time,
+                    object_etag_hash,
+                    object_content_type,
+                    object_content_length,
+                    object_content_encoding,
+                    object_content_disposition,
+                    object_content_language,
+                    object_cache_control,
+                    object_delete_at,
+                    object_manifest_type,
+                    object_manifest,
+                    object_access_control_allow_origin,
+                    object_access_control_allow_credentials,
+                    object_access_control_expose_headers,
+                    object_access_control_max_age,
+                    object_access_control_allow_methods,
+                    object_access_control_allow_headers,
+                    object_origin,
+                    object_access_control_request_method,
+                    object_access_control_request_headers,
+                    object_meta
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %d,
+                    %s, %s, %s, %s, %s, %d, %s, %s, %s, %s, %s, %s, %s, 
+                    %s, %s, %s, %s 
+                )
+                ON DUPLICATE KEY UPDATE
+                    object_uri = VALUES(object_uri),
+                    object_name = VALUES(object_name),
+                    object_account_name = VALUES(object_account_name),
+                    object_container_name = VALUES(object_container_name),
+                    object_location = VALUES(object_location),
+                    object_uri_create_time = VALUES(object_uri_create_time),
+                    object_last_modified_time = VALUES(object_last_modified_time),
+                    object_last_changed_time = VALUES(object_last_changed_time),
+                    object_delete_time = VALUES(object_delete_time),
+                    object_last_activity_time = VALUES(object_last_activity_time),
+                    object_etag_hash = VALUES(object_etag_hash),
+                    object_content_type = VALUES(object_content_type),
+                    object_content_length = VALUES(object_content_length),
+                    object_content_encoding = VALUES(object_content_encoding),
+                    object_content_disposition = VALUES(object_content_disposition),
+                    object_content_language = VALUES(object_content_language),
+                    object_cache_control = VALUES(object_cache_control),
+                    object_delete_at = VALUES(object_delete_at),
+                    object_manifest_type = VALUES(object_manifest_type),
+                    object_manifest = VALUES(object_manifest),
+                    object_access_control_allow_origin = VALUES(object_access_control_allow_origin),
+                    object_access_control_allow_credentials = VALUES(object_access_control_allow_credentials),
+                    object_access_control_expose_headers = VALUES(object_access_control_expose_headers),
+                    object_access_control_max_age = VALUES(object_access_control_max_age),
+                    object_access_control_allow_methods = VALUES(object_access_control_allow_methods),
+                    object_access_control_allow_headers = VALUES(object_access_control_allow_headers),
+                    object_origin = VALUES(object_origin),
+                    object_access_control_request_method = VALUES(object_access_control_request_method),
+                    object_access_control_request_headers = VALUES(object_access_control_request_headers),
+                    object_meta = VALUES(object_meta)
+                ;
+            '''
+            for item in data:
+                formatted_query = query % (
+                    item['object_uri'],
+                    item['object_name'],
+                    item['object_account_name'],
+                    item['object_container_name'],
+                    item['object_location'],
+                    item['object_uri_create_time'],
+                    item['object_last_modified_time'],
+                    item['object_last_changed_time'],
+                    item['object_delete_time'],
+                    item['object_last_activity_time'],
+                    item['object_etag_hash'],
+                    item['object_content_type'],
+                    item['object_content_length'],
+                    item['object_content_encoding'],
+                    item['object_content_disposition'],
+                    item['object_content_language'],
+                    item['object_cache_control'],
+                    item['object_delete_at'],
+                    item['object_manifest_type'],
+                    item['object_manifest'],
+                    item['object_access_control_allow_origin'],
+                    item['object_access_control_allow_credentials'],
+                    item['object_access_control_expose_headers'],
+                    item['object_access_control_max_age'],
+                    item['object_access_control_allow_methods'],
+                    item['object_access_control_allow_headers'],
+                    item['object_origin'],
+                    item['object_access_control_request_method'],
+                    item['object_access_control_request_headers'],
+                    item['object_meta']
                 )
                 conn.executescript(formatted_query)
 
