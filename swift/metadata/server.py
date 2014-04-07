fd91e8a4837a52df29ff3974a67dafaae799a6d3
TJ White <thjwhite@gmail.com>
2014-04-07 16:04:54 -0700

    Implementing superset attributes. Custom not handled right now, but flatgs are set as to wether to include them. Need to be passed into the custom query

--- swift/metadata/server.py
+++ swift/metadata/server.py
@@ -38,6 +38,66 @@ from swift.common.swob import HTTPBadRequest, HTTPConflict, \
     HTTPException
 
 DATADIR = 'metadata'
+ACCOUNT_SYS_ATTRS = [
+    'account_uri',
+    'account_name',
+    'account_tenant_id',
+    'account_first_use_time',
+    'account_last_modified_time',
+    'account_last_changed_time',
+    'account_delete_time',
+    'account_last_activity_time',
+    'account_container_count',
+    'account_object_count',
+    'account_bytes_used']
+
+CONTAINER_SYS_ATTRS = [
+    'container_uri',
+    'container_name',
+    'container_account_name',
+    'container_create_time',
+    'container_last_modified_time',
+    'container_last_changed_time',
+    'container_delete_time',
+    'container_last_activity_time',
+    'container_read_permissions',
+    'container_write_permissions',
+    'container_sync_to',
+    'container_sync_key',
+    'container_versions_location',
+    'container_object_count',
+    'container_bytes_used']
+
+OBJECT_SYS_ATTRS = [
+    'object_uri',
+    'object_name',
+    'object_account_name',
+    'object_container_name',
+    'object_location',
+    'object_uri_create_time',
+    'object_last_modified_time',
+    'object_last_changed_time',
+    'object_delete_time',
+    'object_last_activity_time',
+    'object_etag_hash',
+    'object_content_type',
+    'object_content_length',
+    'object_content_encoding',
+    'object_content_disposition',
+    'object_content_language',
+    'object_cache_control',
+    'object_delete_at',
+    'object_manifest_type',
+    'object_manifest',
+    'object_access_control_allow_origin',
+    'object_access_control_allow_credentials',
+    'object_access_control_expose_headers',
+    'object_access_control_max_age',
+    'object_access_control_allow_methods',
+    'object_access_control_allow_headers',
+    'object_origin',
+    'object_access_control_request_method',
+    'object_access_control_request_headers']
 
 
 class MetadataController(object):
@@ -161,8 +221,8 @@ class MetadataController(object):
                     'account_object_count',
                     'account_bytes_used',
                     'account_meta',
-                    'all_attrs',  # TODO: maybe put these below
-                    'all_system_attrs',  # and handle here
+                    'all_attrs',
+                    'all_system_attrs',
                     'all_meta_attrs',
                     'all_account_attrs'
                     'all_account_system_attrs',
@@ -205,10 +265,9 @@ class MetadataController(object):
         else:
             attrs = "object_uri, container_uri, account_uri"
 
-        if "all_attrs" in attrs.split(','):
-            ret = broker.getAll()
-            status = 200
-        elif self.check_attrs(attrs, acc, con, obj):
+        attrs, all_obj_meta, all_con_meta, all_acc_meta = \
+            eval_superset(attrs.split(","))
+        if self.check_attrs(attrs, acc, con, obj):
             accAttrs, conAttrs, objAttrs, superAttrs, customAttrs = \
                 split_attrs_by_scope(attrs)
 
@@ -291,11 +350,12 @@ class MetadataController(object):
         if not os.path.exists(broker.db_file):
             try:
                 broker.initialize(time.time())
-                created = True
+                # created = True
             except DatabaseAlreadyExists:
-                created = False
+                # created = False
+                pass
         else:
-            created = broker.is_deleted(md_type)
+            #created = broker.is_deleted(md_type)
             # broker.update_put_timestamp(time.time())
             if broker.is_deleted(md_type):
                 return HTTPConflict(request=req)
@@ -320,7 +380,7 @@ class MetadataController(object):
         return HTTPNoContent(request=req)
 
     def __call__(self, env, start_response):
-        start_time = time.time()
+        # start_time = time.time()
         req = Request(env)
         self.logger.txn_id = req.headers.get('x-trans-id', None)
         if not check_utf8(req.path_info):
@@ -346,7 +406,7 @@ class MetadataController(object):
                     'ERROR __call__ error with %(method)s %(path)s '),
                     {'method': req.method, 'path': req.path})
                 res = HTTPInternalServerError(body=traceback.format_exc())
-        trans_time = '%.4f' % (time.time() - start_time)
+        # trans_time = '%.4f' % (time.time() - start_time)
         # if self.log_requests:
         #     log_message = '%s - - [%s] "%s %s" %s %s "%s" "%s" "%s" %s' % (
         #         req.remote_addr,
@@ -398,3 +458,69 @@ def app_factory(global_conf, **local_conf):
     conf = global_conf.copy()
     conf.update(local_conf)
     return MetadataController(conf)
+
+
+def eval_superset(attrs):
+    """
+    Take in the list of attrs and iterate through
+    the list and if there is a superset attrs
+    replace it with the set of attrs represented
+    through by the superset attr
+
+    If custom metadata superset attr is included,
+    also return a boolean for wether or not
+    to include all metadata for obj/con/acc
+    since it is not possible to add these
+    to the attr list. These flags will
+    be used later in the query to the
+    custom table
+
+    Returns the expanded attrs as a string.
+    """
+    expanded = set()
+    cust_obj = False
+    cust_con = False
+    cust_acc = False
+    for attr in attrs:
+        if attr == "all_attrs":
+            expanded = expanded.union(set(ACCOUNT_SYS_ATTRS))
+            expanded = expanded.union(set(CONTAINER_SYS_ATTRS))
+            expanded = expanded.union(set(OBJECT_SYS_ATTRS))
+            cust_acc = True
+            cust_con = True
+            cust_obj = True
+        elif attr == "all_system_attrs":
+            expanded = expanded.union(set(ACCOUNT_SYS_ATTRS))
+            expanded = expanded.union(set(CONTAINER_SYS_ATTRS))
+            expanded = expanded.union(set(OBJECT_SYS_ATTRS))
+        elif attr == "all_meta_attrs":
+            cust_acc = True
+            cust_con = True
+            cust_obj = True
+
+        elif attr == "all_account_attrs":
+            expanded = expanded.union(set(ACCOUNT_SYS_ATTRS))
+            cust_acc = True
+        elif attr == "all_account_system_attrs":
+            expanded = expanded.union(set(ACCOUNT_SYS_ATTRS))
+        elif attr == "all_account_meta_attrs":
+            cust_acc = True
+
+        elif attr == "all_container_attrs":
+            expanded = expanded.union(set(CONTAINER_SYS_ATTRS))
+            cust_con = True
+        elif attr == "all_container_system_attrs":
+            expanded = expanded.union(set(CONTAINER_SYS_ATTRS))
+        elif attr == "all_container_meta_attrs":
+            cust_con = True
+
+        elif attr == "all_object_attrs":
+            expanded = expanded.union(set(OBJECT_SYS_ATTRS))
+            cust_obj = True
+        elif attr == "all_object_system_attrs":
+            expanded = expanded.union(set(OBJECT_SYS_ATTRS))
+        elif attr == "all_object_meta_attrs":
+            cust_obj = True
+        else:
+            expanded.add(attr)
+    return (",".join(list(expanded)), cust_obj, cust_con, cust_acc)
